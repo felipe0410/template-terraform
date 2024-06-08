e544344bcdb9f4d7663e6aa4099f0f59fe3d1a02
Felipe Montana <andres.montana.r4@gmail.com>
2024-06-07 19:21:20 -0500

    correction definition accounts

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.0"
-    }
-  }
-}
-provider "aws" {
-}
-
 resource "aws_s3_bucket" "b" {
   bucket = "mybucket-github-tf"
-  tags = {
-    Account       = "My account"
-    Application   = "Application"
-    SAP-CC        = 123123,
-    ApplicationCI = 3453453
-  }
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "APPLICATION_CI" {}
+variable "SAP_CC" {}
+variable "OWNER" {}
+variable "ACCOUNT" {}
